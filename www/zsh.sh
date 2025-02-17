a2c92e529b6c7b5903f1ac6c769881d8632fb442
Leon Hubrich <github@gamescrack.net>
2025-02-17 22:33:43 +0100

    Fix up the installer.

--- www/zsh.sh
+++ www/zsh.sh
@@ -162,7 +162,7 @@ main() {
     # Download configuration files
     curl -s "$BASEURL/theme.omp.json" -o "$DSTPATH/theme.omp.json"
     curl -s "$BASEURL/zshrc" -o "$HOME/.zshrc"
-    curl -s https://api.github.com/repos/Games-Crack/shellconfig/commits | jq -r '.[0].sha' > "$DSTPATH/version"
+    curl -s "$BASEURL/latest_version" | jq -r '.[0].sha' > "$DSTPATH/version"
 
     # Change default shell to Zsh
     chsh -s $(which zsh)
